4f8fd3e4787f8ec3734eb90e8f70ab13ef726a85
shinigamirei <shinigamireyas@googlemail.com>
2019-06-24 12:15:03 +0100

    added population of apartment list with data from backend

--- src/components/apartment_list.component.js
+++ src/components/apartment_list.component.js
@@ -8,12 +8,11 @@ export default class ApartmentList extends React.Component {
         super(props);
 
         this.state = {
-            data: {
-                Headers: ['Apartment Name', 'Apartment Address', 'Apartment Region', 'Apartment Rooms'],
-                Rows: []
-            },
-            databaseresponse: []
             
+            Headers: ['Apartment Name', 'Apartment Address', 'Apartment Region', 'Apartment Rooms'],
+            Rows: [],
+            databaseresponse: []
+
         };
 
 
@@ -22,7 +21,7 @@ export default class ApartmentList extends React.Component {
     componentDidMount() {
         axios.get('http://localhost:4000/apartment/getAll')
             .then(response => {
-                this.setState({ databaseresponse: response.data })
+                this.setState({ databaseresponse: response.data }, console.log(response.data))
 
             })
             .catch(function (error) {
@@ -33,15 +32,18 @@ export default class ApartmentList extends React.Component {
 
     render() {
         let json = this.state.data
+        let databaseJson = []
 
-        let databaseJson = [{ "_id": "5d08a0a945b6800f9066472d", "apartment_name": "Hell", "apartment_address": "6 6 6", "apartment_region": "Manchester", "apartment_rooms": [], "__v": 0 },
-        { "_id": "5d08a21621ef1831c0ced8d1", "apartment_name": "Hed", "apartment_address": "7 6 6", "apartment_region": "Manchester", "apartment_rooms": [{ "room_occupancies": [], "_id": "5d08a23921ef1831c0ced8d2", "room_name_number": "111" }, { "room_occupancies": [], "_id": "5d08a31021ef1831c0ced8d3", "room_name_number": "112" }, { "room_occupancies": [], "_id": "5d08a31421ef1831c0ced8d4", "room_name_number": "113" }], "__v": 3 }]
+        let datares = []
+        axios.get('http://localhost:4000/apartment/getAll')
+            .then(Response => { databaseJson = Response.data })
+
 
         let headers = ['Apartment Name', 'Apartment Address', 'Apartment Region', 'Apartment Rooms']
         let rows = []
 
         //Creates a row for each apartment Json
-        databaseJson.map(data => {
+        this.state.databaseresponse.map(data => {
             let rooms = []
             //Gets all the room numbers in the apartment and adds to room array
             for (var room in data.apartment_rooms) {
