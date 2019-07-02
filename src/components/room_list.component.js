2085523c9c33c131f60da71166975cddf52f0707
shinigamirei <shinigamireyas@googlemail.com>
2019-07-02 11:13:10 +0100

    updated room list to use new table and properly populate it with rooms from the database

--- src/components/room_list.component.js
+++ src/components/room_list.component.js
@@ -1,30 +1,61 @@
-import React from "react";
+import React, { Component } from 'react';
 import QATable from './Generics/qa-table.component';
+import axios from 'axios';
 
-export default class ApartmentList extends React.Component {
+export default class RoomList extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            data: {
-                Headers: ['Apartment number', 'Room number', 'Button'],
-                Rows: [{ 'Apartment number': '22', 'Room number': '1', 'Button': <button>Click Me!</button> },
-                { 'Apartment number': '26', 'Room number': '1', 'Button': <button>No, Click Me!</button> }]
-            }
+            Rows: [],
+            databaseresponse: []
         };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/apartment/getAll')
+            .then(response => {
+                this.setState({ databaseresponse: response.data })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
     render() {
 
-        let json = this.state.data
+        let headers = [{ 'header': 'Room Number', 'width': 400 }, { 'header': 'Room Occupancies', 'width': 400 },
+        { 'header': 'Room Availability', 'width': 400 }, { 'header': 'Assign Trainee', 'width': 200 }]
+        let rows = []
+
+        //Creates a row for each apartment Json
+        this.state.databaseresponse.map(data => {
+            //Gets all the room numbers in the apartment and adds to room array
+            for (var room in data.apartment_rooms) {
+               
+
+                let row = {
+                    'Room Number': data.apartment_rooms[room].room_name_number,
+                    'Room Occupancies': data.apartment_rooms[room].room_occupancies,
+                    'Room Availability': "available",
+                    'Assign Trainee': <button>Assign</button>
+                }
+                //Adds apartment row to Rows
+                rows.push(row)
+            }
+        })
+
+        //This what you give the table component
+        let tableData = { Headers: headers, Rows: rows }
 
         return (
             <div>
                 <h2>
                     Room list
                 </h2>
-                <QATable data={json} />
+                <QATable data={tableData} />
+
             </div>
         );
     };
