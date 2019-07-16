7647e769ce5fa817d184356fd73b0d3ad7309aae
Daniel <d.olugbade@outlook.com>
2019-07-16 11:58:10 +0100

    Updated Textb.. comp updated table css, added icon

--- src/components/Generics/text-button.component.js
+++ src/components/Generics/text-button.component.js
@@ -21,11 +21,12 @@ export default class TextButton extends Component {
             <div style={styles}>
                 <ButtonGroup role={this.props.role ? this.props.role : 'group'}>
                     <Button
-                        id={this.props.id ? this.props.id : 'Btn'}
+                        id={this.props.className ? this.props.className : 'Btn'}
                         type={this.props.type}
                         onClick={this.props.onClick}
-                        disabled={this.props.disabled}>
-                        {this.props.text}
+                        disabled={this.props.disabled}
+						data-arg1={this.props.dataarg1}>
+                        {this.props.children}<img src={this.props.icon}></img>
                     </Button>
                 </ButtonGroup>
             </div>
