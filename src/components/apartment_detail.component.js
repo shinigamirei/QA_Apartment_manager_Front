6741571477c13325db37a1566c2f4083f32389f8
Daniel <d.olugbade@outlook.com>
2019-07-19 09:49:50 +0100

    Document format updated

--- src/components/apartment_detail.component.js
+++ src/components/apartment_detail.component.js
@@ -9,20 +9,20 @@ export default class ApartmentDetail extends React.Component {
 
     constructor(props) {
         super(props);
-      
+
         this.state = {
-			showForm_AssignTrainee : false
-        };  
+            showForm_AssignTrainee: false
+        };
         this.handleButtonAddOccupany = this.handleButtonAddOccupany.bind(this);
         this.handleButtonCloseOccupany = this.handleButtonCloseOccupany.bind(this);
 
-	}
-	
+    }
+
     handleButtonAddOccupany(e) {
-       this.setState({showForm_AssignTrainee: true});
+        this.setState({ showForm_AssignTrainee: true });
     }
     handleButtonCloseOccupany(e) {
-       this.setState({showForm_AssignTrainee: false});
+        this.setState({ showForm_AssignTrainee: false });
     }
     render() {
         console.log(this.props.role)
@@ -52,9 +52,9 @@ export default class ApartmentDetail extends React.Component {
             }
 
         ]
-        
-        
-                let issues = [
+
+
+        let issues = [
             {
                 "id": "565",
                 "name": "dummy name of issue",
@@ -83,17 +83,17 @@ export default class ApartmentDetail extends React.Component {
         return (
             <div>
 
-            <div>
+                <div>
                 </div>
                 <span style={{ float: "right" }}>
-            	<TextButton float="right" id="AddOccupancy" onClick={this.handleButtonAddOccupany}>Add occupant</TextButton><br/>
-				{this.state.showForm_AssignTrainee && 
-					<div>
-						<AddOccupancy float="right" _id={_aprtDetail["ID"]} apartment={_aprtDetail["Apartment Number"]}/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-						<TextButton float="right"  id="Close" onClick={this.handleButtonCloseOccupany}>Close</TextButton>
-					</div>
-				}
-				</span>
+                    <TextButton float="right" id="AddOccupancy" onClick={this.handleButtonAddOccupany}>Add occupant</TextButton><br />
+                    {this.state.showForm_AssignTrainee &&
+                        <div>
+                            <AddOccupancy float="right" _id={_aprtDetail["ID"]} apartment={_aprtDetail["Apartment Number"]} /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                            <TextButton float="right" id="Close" onClick={this.handleButtonCloseOccupany}>Close</TextButton>
+                        </div>
+                    }
+                </span>
 
                 <span style={{ display: "inline-flex" }}>
 
@@ -105,39 +105,39 @@ export default class ApartmentDetail extends React.Component {
                         <div><h3 align="center">{JSON.stringify(_aprtDetail["Apartment Number"]).replace(/\"/g, "")}</h3></div>
                         <div><h2 align="center">{JSON.stringify(_aprtDetail["Apartment Address"]).replace(/\"/g, "")}</h2></div>
                     </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    
+
                 <h3 style={{ position: "relative", left: "42px", top: "45px" }}>{JSON.stringify(_aprtDetail.Availability).replace(/\"/g, "")}</h3>
                 </span>
                 <hr />
-                  
-                  <div>
+
+                <div>
                     <div><h2>Occupiers</h2></div>
                     <div>
                         <ul style={{ lineHeight: "15px" }}>
                             {occupiers
                                 .map(
                                     occupiers =>
-                                    <li value={occupiers} key={occupiers} style={{ listStyleType: "none", marginLeft: "-40px" }}>
-                                    <span style={{ display: "inline-flex" }}>
-                                        <p style={{ fontSize: "large" }}>
-                                            {`${occupiers.f_name} ${occupiers.l_name}, `}
-                                        </p>&nbsp;&nbsp;&nbsp;
-
+                                        <li value={occupiers} key={occupiers} style={{ listStyleType: "none", marginLeft: "-40px" }}>
+                                            <span style={{ display: "inline-flex" }}>
+                                                <p style={{ fontSize: "large" }}>
+                                                    {`${occupiers.f_name} ${occupiers.l_name}, `}
+                                                </p>&nbsp;&nbsp;&nbsp;
+        
                                     <p style={{ fontSize: "large" }}>
-                                            <b> Phone Number:  </b> {occupiers.phone_number},
+                                                    <b> Phone Number:  </b> {occupiers.phone_number},
                                     </p>&nbsp;&nbsp;&nbsp;
-
+            
                                     <p style={{ fontSize: "large" }}>
-                                            <b> Dates of Occupancy:  </b> {occupiers.start_date} - {occupiers.end_date}
-                                        </p>
-                                    </span>
-                                </li>)}
+                                                    <b> Dates of Occupancy:  </b> {occupiers.start_date} - {occupiers.end_date}
+                                                </p>
+                                            </span>
+                                        </li>)}
                         </ul>
                     </div>
                 </div>
-                <hr/>
+                <hr />
 
-<div>
+                <div>
                     <div><h2>Issues</h2></div>
                     <div>
                         <ul style={{ lineHeight: "15px" }}>
@@ -166,7 +166,7 @@ export default class ApartmentDetail extends React.Component {
 
 
 
-            </div>
+                </div>
             </div>
         );
     }
